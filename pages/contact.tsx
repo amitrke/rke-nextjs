f93a501d85f44b5fe00eb997d99abb715ade60bb
amitrke <amitrke@gmail.com>
2022-12-10 22:49:50 +0000

    :bug:Fixed lint error

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -45,7 +45,7 @@ export default function Contact() {
                         <Form.Label>Email address</Form.Label>
                         <Form.Control required type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                            We&apos;ll never share your email with anyone else.
                         </Form.Text>
                         <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                     </Form.Group>
