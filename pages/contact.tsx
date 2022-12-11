023fb68eaf07b9442dbe367b773c2ceb7e4296c8
amitrke <amitrke@gmail.com>
2022-12-11 01:42:16 +0000

    Updated message save

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { write } from "../firebase/firestore";
 import { MessageType } from "../firebase/types";
@@ -10,8 +10,14 @@ export default function Contact() {
     const { user } = useUser();
 
     const [message, setMessage] = useState<MessageType>({
-        fromUserId: '', title: '', body: '', updateDate: (new Date()).getTime(), toUserId: '', state: 'unread', thread: ''
+        fromUserId: user?.id, title: '', body: '', updateDate: (new Date()).getTime(), toUserId: '', state: 'unread', thread: ''
     })
+
+    useEffect(() => {
+        if (user && user.id) {
+            setMessage({ ...message, fromUserId: user.id });
+        }
+    }, [user])
 
     const handleSubmit = async (event) => {
         const form = event.currentTarget;
@@ -19,74 +25,43 @@ export default function Contact() {
             event.preventDefault();
             event.stopPropagation();
         }
-
         const doc = await write({ path: `messages`, data: message });
+        //console.dir(message);
         setValidated(true);
         setSubmitted(true);
     };
 
-    const ContactForm = () => {
-        return (
-            <>
-                <h1>Contact</h1>
-                <Form validated={validated} onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control required type="text" placeholder="Enter your name" value={album.name} onChange={(e) => { setAlbum({ ...album, name: e.target.value }) }} />
-                        <Form.Text className="text-muted">
-                            Your Name.
-                        </Form.Text>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formReason">
-                        <Form.Label>Reason</Form.Label>
-                        <Form.Select aria-label="Default select example">
-                            <option value="General">General</option>
-                            <option value="Adv">Post an advertisement</option>
-                            <option value="Dev">Contribute to the website as a developer</option>
-                            <option value="Defect">Report an issue with website</option>
-                        </Form.Select>
-                        <Form.Text className="text-muted">
-                            Select the reason why you want to contact the website team.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control required type="email" placeholder="Enter email" value={user?.email} />
-                        <Form.Text className="text-muted">
-                            We&apos;ll never share your email with anyone else.
-                        </Form.Text>
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formTextArea">
-                        <Form.Label>Message</Form.Label>
-                        <Form.Control required as="textarea" rows={3} />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </>
-        )
-    }
-
-    const ThanksView = () => {
-        return (
-            <>
-            <p>Thanks for reaching out.</p>
-            </>
-        )
-    }
-
-    const PageContent = (props) => {
-        if (submitted) return ThanksView();
-        else return ContactForm();
-    }
-
     return (
         <Container>
-            <PageContent />
+            <h1>Contact</h1>
+            <Form validated={validated} onSubmit={handleSubmit} className={submitted ? 'hidden' : undefined}>
+                <Form.Group className="mb-3" controlId="formReason">
+                    <Form.Label>Reason</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option value="General">General</option>
+                        <option value="Adv">Post an advertisement</option>
+                        <option value="Dev">Contribute to the website as a developer</option>
+                        <option value="Defect">Report an issue with website</option>
+                    </Form.Select>
+                    <Form.Text className="text-muted">
+                        Select the reason why you want to contact the website team.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formTextArea">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control required as="textarea" rows={3} defaultValue={message.body} onChange={(e) => { setMessage({ ...message, body: e.target.value }) }} />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p className={submitted ? undefined : 'hidden'}>
+                Thanks for reaching out!
+            </p>
+            <p className={user ? 'hidden' : undefined}>
+                Please Login!
+            </p>
         </Container>
     )
 }
