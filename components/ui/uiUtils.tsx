b73e3f83e78f0461450bc3c7dbe8e29b432cd789
Amit Kumar <amitrke@gmail.com>
2023-03-18 16:58:57 -0700

    :sparkles:Weather detail page

--- components/ui/uiUtils.tsx
+++ components/ui/uiUtils.tsx
@@ -1,3 +1,7 @@
 export const uiDateFormat = (datetime: number) => {
     return new Date(datetime).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"})
 }
+
+export const uiRound = (value: number, precision?: number) => {
+    return Math.round(value * (precision || 1)) / (precision || 1);
+}
