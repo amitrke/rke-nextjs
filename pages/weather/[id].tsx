07d815517a701cfac4ce5214496c49c1e945c9ee
Amit Kumar <amitrke@gmail.com>
2023-03-24 15:32:13 -0700

    Fixed weather layout

--- pages/weather/[id].tsx
+++ pages/weather/[id].tsx
@@ -5,12 +5,12 @@ import { uiRound } from '../../components/ui/uiUtils';
 
 function dateTimeFormat(date: number, timezone_offset: number, timeZone: string): string {
     const d = new Date(date * 1000);
-    return d.toLocaleString('en-IN', { timeZone, weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', hour: '2-digit', minute:'2-digit' });
+    return d.toLocaleString('en-IN', { timeZone, weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit' });
 }
 
 function timeFormat(date: number, timezone_offset: number, timeZone: string): string {
     const d = new Date(date * 1000);
-    return d.toLocaleString('en-IN', {hour: '2-digit', minute:'2-digit', timeZone });
+    return d.toLocaleString('en-IN', { hour: '2-digit', minute: '2-digit', timeZone });
 }
 
 function dayOfWeekFormat(date: number, timezone_offset: number, timeZone: string): string {
@@ -77,63 +77,74 @@ type Weather = {
     daily: DailyWeather[];
 }
 
+//Current Weather Widget
+function CurrentWeatherWidget(props: CurrentWeather) {
+    return (
+        <Card className="mb-4 gradient-custom" style={{ borderRadius: "25px" }}>
+            <Card.Body className="p-4">
+                <div className="d-flex justify-content-between mb-4 pb-2">
+                    <div>
+                        <h2 className="display-2"><strong>{uiRound(props.temp, 1)}°C</strong></h2>
+                        <p className="text-muted mb-0">Roorkee, India.</p>
+                        <p className="text-muted mb-0">{props.weather[0].description}</p>
+                        <p className="text-muted mb-0">{dateTimeFormat(props.dt, 0, 'Asia/Kolkata')}</p>
+                    </div>
+                    <div>
+                        <Image src={'https://openweathermap.org/img/wn/' + props.weather[0].icon + '@2x.png'} />
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
+//Hourly Weather Widget
+function HourlyWeatherWidget(props: Weather) {
+    return (
+        <Card className="mb-4 gradient-custom" style={{ borderRadius: "25px" }}>
+            <Card.Body className="p-4">
+                <div className="d-flex justify-content-around text-center mb-4 pb-3 pt-2">
+                    {[...props.hourly].slice(1, 7).map((hrWthr, i) =>
+                        // Hide div for small screens when i > 2
+                        <div className={"flex-column " + (i > 2 ? "d-none d-md-block" : "")} key={i}>
+                            <p className="small"><strong>{uiRound(hrWthr.temp, 1)}°C</strong></p>
+                            <Image src={'https://openweathermap.org/img/wn/' + hrWthr.weather[0].icon + '@2x.png'} />
+                            <p className="mb-0"><strong>{timeFormat(hrWthr.dt, props.timezone_offset, props.timezone)}</strong></p>
+                        </div>
+                    )}
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
+//Daily Weather Widget
+function DailyWeatherWidget(props: Weather) {
+    return (
+        <Card className="mb-4 gradient-custom" style={{ borderRadius: "25px" }}>
+            <Card.Body className="p-4">
+                <div className="d-flex justify-content-around text-center mb-4 pb-3 pt-2">
+                    {[...props.daily].slice(1, 6).map((dlWthr, i) =>
+                        <div className={"flex-column " + (i > 2 ? "d-none d-md-block" : "")} key={i}>
+                            <p className="small"><strong>{uiRound(dlWthr.temp.day, 1)}°C</strong></p>
+                            <Image src={'https://openweathermap.org/img/wn/' + dlWthr.weather[0].icon + '@2x.png'} />
+                            <p className="mb-0"><strong>{dayOfWeekFormat(dlWthr.dt, props.timezone_offset, props.timezone)}</strong></p>
+                        </div>
+                    )}
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
 export default function Weather(props: Weather) {
-    // console.log(props);
     return (
         <Container fluid className="vh-100" style={{ backgroundColor: '#C1CFEA' }}>
             <Row className="h-100 d-flex justify-content-center align-items-center" style={{ color: '#282828' }}>
                 <Col md={9} lg={7} xl={5}>
-                    <div className="card mb-4 gradient-custom" style={{ borderRadius: "25px" }}>
-                        <div className="card-body p-4">
-                            <Carousel id="demo1" interval={null} slide={false}>
-                                <Carousel.Item>
-                                    <div className="d-flex justify-content-between mb-4 pb-2">
-                                        <div>
-                                            <h2 className="display-2"><strong>{uiRound(props.current.temp, 1)}°C</strong></h2>
-                                            <p className="text-muted mb-0">Roorkee, India.</p>
-                                            <p className="text-muted mb-0">{props.current.weather[0].description}</p>
-                                            <p className="text-muted mb-0">{dateTimeFormat(props.current.dt, props.timezone_offset, props.timezone)}</p>
-                                        </div>
-                                        <div>
-                                            <Image src={'https://openweathermap.org/img/wn/' + props.current.weather[0].icon + '@2x.png'} />
-                                        </div>
-                                    </div>
-                                </Carousel.Item>
-                            </Carousel>
-                        </div>
-                    </div>
-                    <Card className="mb-4" style={{ borderRadius: '25px' }}>
-                        <Card.Body className="p-4">
-                            <div id="demo2" className="carousel slide" data-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <div className="d-flex justify-content-around text-center mb-4 pb-3 pt-2">
-                                            {[...props.hourly].slice(1, 7).map((hrWthr, i) =>
-                                                <div className="flex-column" key={i}>
-                                                    <p className="small"><strong>{uiRound(hrWthr.temp, 1)}°C</strong></p>
-                                                    <Image src={'https://openweathermap.org/img/wn/' + hrWthr.weather[0].icon + '@2x.png'} />
-                                                    <p className="mb-0"><strong>{timeFormat(hrWthr.dt, props.timezone_offset, props.timezone)}</strong></p>
-                                                </div>
-                                            )}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                    <Carousel style={{ borderRadius: 25 }}>
-                        <Carousel.Item>
-                            <div className="d-flex justify-content-around text-center mb-4 pb-3 pt-2">
-                            {[...props.daily].slice(1, 6).map((dlWthr, i) =>
-                                <div className="flex-column" key={i}>
-                                    <p className="small"><strong>{uiRound(dlWthr.temp.day, 1)}°C</strong></p>
-                                    <Image src={'https://openweathermap.org/img/wn/' + dlWthr.weather[0].icon + '@2x.png'} />
-                                    <p className="mb-0"><strong>{dayOfWeekFormat(dlWthr.dt, props.timezone_offset, props.timezone)}</strong></p>
-                                </div>
-                            )}
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
+                    <CurrentWeatherWidget {...props.current} />
+                    <HourlyWeatherWidget {...props} />
+                    <DailyWeatherWidget {...props} />
                 </Col>
             </Row>
         </Container>
