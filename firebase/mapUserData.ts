ebac16e20936325ca632dbaca5907a95f43a7764
Amit Kumar <amitrke@gmail.com>
2023-10-14 09:59:14 -0700

    Fixed user info update

--- firebase/mapUserData.ts
+++ firebase/mapUserData.ts
@@ -1,12 +1,12 @@
 import { User } from "./types"
 
 export const mapUserData = (user): User => {
-    const { uid, email, xa, displayName, photoUrl } = user
+    const { uid, email, xa, displayName, photoURL } = user
     return {
         id: uid,
         email,
         token: xa,
         name: displayName,
-        profilePic: photoUrl
+        profilePic: photoURL
     }
 }
