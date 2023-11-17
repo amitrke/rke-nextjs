5426eb604cbdf0d239731a34854a4eb0e82bb924
Amit Kumar <amitrke@gmail.com>
2023-11-17 15:07:44 -0800

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,33 +3,67 @@ import '../styles/main.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from '../components/layout'
 import { SSRProvider } from '@react-aria/ssr';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   if (Component.noLayout) {
     return (
       <>
-          <Component {...pageProps} />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KVDNNXYM2C" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-KVDNNXYM2C');
+        `}
+        </Script>
+        <Component {...pageProps} />
       </>
     )
   } else if (Component.noSSR) {
     return (
       <>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KVDNNXYM2C" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-KVDNNXYM2C');
+        `}
+        </Script>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </>
     )
-  } 
+  }
   else {
     return (
-      <SSRProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SSRProvider>
+      <>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KVDNNXYM2C" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-KVDNNXYM2C');
+        `}
+        </Script>
+        <SSRProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SSRProvider>
+      </>
+
     )
   }
-  
+
 }
 
 export default MyApp
